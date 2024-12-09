44ec107191177fda7c87a670fec144bf1f0da8ba
Sylvain Filoni <fffiloni@users.noreply.huggingface.co>
2024-12-09 17:30:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def run_inference(temp_dir):
         )
 
         # Collect the output images
-        output_images = glob(os.path.join(temp_dir, "*.png"))
+        output_images = glob(os.path.join("mv_results", "*.mp4"))
         return output_images
     except subprocess.CalledProcessError as e:
         return f"Error during inference: {str(e)}"
@@ -96,19 +96,15 @@ def process_image(input_url):
     removed_bg_path, temp_dir = result  # Unpack only if successful
 
     # Run inference
-    output_images = run_inference(temp_dir)
+    output_video = run_inference(temp_dir)
 
-    if isinstance(output_images, str) and output_images.startswith("Error"):
+    if isinstance(output_video, str) and output_video.startswith("Error"):
         shutil.rmtree(temp_dir)
         raise gr.Error(f"{output_images}")   # Return the error message if inference failed
 
-    # Prepare outputs for display
-    results = []
-    for img_path in output_images:
-        results.append((img_path, img_path))
-
-    #shutil.rmtree(temp_dir)  # Cleanup temporary folder
-    return results
+    
+    shutil.rmtree(temp_dir)  # Cleanup temporary folder
+    return output_video
 
 def gradio_interface():
     with gr.Blocks() as app:
@@ -118,9 +114,9 @@ def gradio_interface():
             input_image = gr.Image(label="Image input", type="filepath")
             submit_button = gr.Button("Process")
 
-        output_gallery = gr.Gallery(label="Output Images")
+        output_video= gr.Video(label="Output Video")
 
-        submit_button.click(process_image, inputs=[input_image], outputs=[output_gallery])
+        submit_button.click(process_image, inputs=[input_image], outputs=[output_video])
 
     return app
 
